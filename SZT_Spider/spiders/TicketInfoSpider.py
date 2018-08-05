2265a8d5e420152aa2cb4209112a4431f27d8b68
laiwenqiang <1260091093@qq.com>
2018-08-05 17:38:09 +0800

    xpath to get amt

--- SZT_Spider/spiders/TicketInfoSpider.py
+++ SZT_Spider/spiders/TicketInfoSpider.py
@@ -5,8 +5,18 @@ import scrapy
 class TicketinfospiderSpider(scrapy.Spider):
     name = 'TicketInfoSpider'
     allowed_domains = ['shenzhentong.com']
-    start_urls = ['http://query.shenzhentong.com:8080/sztnet/qrycard.jsp']
 
-    def parse(self, response):
-        print response.text
+    def start_requests(self):
+        url = 'http://query.shenzhentong.com:8080/sztnet/qryCard.do'
+
+        yield scrapy.FormRequest(
+            url=url,
+            formdata={"cardno": "698474623"},  # TODO 目前cardno写死
+            callback=self.parse_page
+        )
+
+    def parse_page(self, response):
+        # print response.text
+        for val in response.xpath('//td[@id="cardRealAmt"]/text()').extract():  # 数组
+            print val
         pass
